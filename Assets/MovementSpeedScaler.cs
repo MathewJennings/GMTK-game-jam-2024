280413205b176808e1802e7166215e0d6eb776eb
Matthew Palmer <matthewpalmer99@gmail.com>
2024-08-16 23:57:43 -0400

    Adding movement scaling to scaler prefab and gun shoot script

---
+++ Assets/MovementSpeedScaler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementSpeedScaler : MonoBehaviour
+{
+    [SerializeField]
+    private float multiplier = 1f;
+
+    public float PollMultipler()
+    {
+        float currentMultiplier = multiplier;
+        return currentMultiplier;
+    }
+
+    public float StealMultipler()
+    {
+        float currentMultiplier = multiplier;
+        UpdateMultiplier(1f);
+        return currentMultiplier;
+    }
+
+    public void SwapMultiplier(MovementSpeedScaler source)
+    {
+        float currentMultiplier = multiplier;
+        UpdateMultiplier(source.PollMultipler());
+        source.UpdateMultiplier(currentMultiplier);
+    }
+
+    public void UpdateMultiplier(float newMultiplier)
+    {
+        multiplier = newMultiplier;
+    }
+}
