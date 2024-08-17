2b03699e38f5ae94f39db4c593eea09894c5f87d
Felix Lee <flee135@users.noreply.github.com>
2024-08-17 00:15:53 -0700

    Add text placeholder to gun displaying ammo

--- Assets/Scripts/Gun/GunRotate.cs
+++ Assets/Scripts/Gun/GunRotate.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class GunRotate : MonoBehaviour
 {
     Camera camera;
+
+    [SerializeField]
+    private Transform tip;
+    [SerializeField]
+    private Canvas canvas; 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +20,15 @@ public class GunRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Set rotation of gun.
         Vector3 mousePos = Input.mousePosition;
         // Offset the camera's z position.
         mousePos.z = camera.transform.position.z * -1;
         mousePos = camera.ScreenToWorldPoint(mousePos);
         transform.right = mousePos - transform.position;
+
+        // Position the canvas above the tip of the gun and rotate it rightside up.
+        canvas.transform.position = tip.transform.position + new Vector3(0f, 0.5f, 0f);
+        canvas.transform.rotation = camera.transform.rotation;
     }
 }
