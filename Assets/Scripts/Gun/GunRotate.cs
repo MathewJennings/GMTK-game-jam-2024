9d31430c317659f8e25d3a06aaf9245d04782be8
Felix Lee <flee135@users.noreply.github.com>
2024-08-17 01:01:29 -0700

    Display correct gun ammo amounts

--- Assets/Scripts/Gun/GunRotate.cs
+++ Assets/Scripts/Gun/GunRotate.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GunRotate : MonoBehaviour
@@ -7,9 +8,13 @@ public class GunRotate : MonoBehaviour
     Camera camera;
 
     [SerializeField]
+    private Transform player;
+    [SerializeField]
     private Transform tip;
     [SerializeField]
-    private Canvas canvas; 
+    private Canvas canvas;
+    [SerializeField]
+    private TMP_Text text;
 
     // Start is called before the first frame update
     void Start()
@@ -30,5 +35,23 @@ public class GunRotate : MonoBehaviour
         // Position the canvas above the tip of the gun and rotate it rightside up.
         canvas.transform.position = tip.transform.position + new Vector3(0f, 0.5f, 0f);
         canvas.transform.rotation = camera.transform.rotation;
+
+        // Set the text for the canvas
+        GunShoot.Mode currentMode = tip.GetComponent<GunShoot>().GetMode();
+        if (canvas.transform.childCount == 1) // Sanity check there's one child of canvas, which we expect is text.
+        {
+            if (currentMode == GunShoot.Mode.SizeScale)
+            {
+                SizeScaler sizeScaler = player.GetComponent<SizeScaler>();
+                float sizeMult = sizeScaler.GetTransformScaleMultiplier();
+                text.SetText("Size\n" + sizeMult + "x");
+            }
+            else if (currentMode == GunShoot.Mode.MovementSpeedScale)
+            {
+                MovementSpeedScaler movementSpeedScaler = player.GetComponent<MovementSpeedScaler>();
+                float speedMult = movementSpeedScaler.getMultiplier();
+                text.SetText("Speed\n" + speedMult + "x");
+            }
+        }
     }
 }
