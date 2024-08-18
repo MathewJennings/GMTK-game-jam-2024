e5c5df1f442630787683fe235ef7d4794c0c8831
Matthew Palmer <matthewpalmer99@gmail.com>
2024-08-18 00:41:35 -0400

    fixing bug where player walk animation would play on moving platforms

--- Assets/PlayerAnimator.cs
+++ Assets/PlayerAnimator.cs
@@ -13,7 +13,8 @@ public class PlayerAnimator : MonoBehaviour
 
     void Update()
     {
-        animator.SetFloat("Speed", Mathf.Abs(body.velocity.x));
+        bool isPlayerWalking = (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A));
+        animator.SetBool("isPlayerWalking", isPlayerWalking);
         animator.SetBool("isOnGround", jump.IsPlayerOnGround());
     }
 }
