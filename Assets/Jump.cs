789e1aac5d4f4eb85ad88f4127b33e83dbcf5fc0
betakyros <zheng.franklin@gmail.com>
2024-08-17 21:29:40 -0400

    Remove the ability to wall jump unless you are near the top

---
+++ Assets/Jump.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    [SerializeField]
+    GameObject jumper;
+    [SerializeField]
+    private float jumpMult = 50;
+
+    public AudioSource jumpAudioSource;
+
+    private HashSet<GameObject> groundsBeingTouched = new();
+    private bool startedAJump = false;
+    private float timeLastTouchedGround = -10f;
+    private float coyoteTimeAmount = .2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) &&
+            (groundsBeingTouched.Count > 0 || (Time.time - timeLastTouchedGround < coyoteTimeAmount)))
+        {
+            startedAJump = true;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Rigidbody2D rb = jumper.GetComponent<Rigidbody2D>();
+
+        if (startedAJump)
+        {
+            PlayRandomizedJumpSound();
+            rb.AddForce(Vector2.up * jumpMult, ForceMode2D.Impulse);
+            // Reset jump state
+            startedAJump = false;
+            timeLastTouchedGround = -10f;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("jump collision enter");
+        CheckForGroundCollision(collision.gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("jump trigger enter");
+        CheckForGroundCollision(collision.gameObject);
+    }
+
+    private void CheckForGroundCollision(GameObject gameObject)
+    {
+        if (gameObject.GetComponent<JumpableSurface>() != null)
+        {
+            groundsBeingTouched.Add(gameObject);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        CheckForGroundExit(collision.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        CheckForGroundExit(collision.gameObject);
+    }
+
+    private void CheckForGroundExit(GameObject gameObject)
+    {
+        if (gameObject.GetComponent<JumpableSurface>() != null)
+        {
+            groundsBeingTouched.Remove(gameObject);
+            if (groundsBeingTouched.Count == 0)
+            {
+                //dont retrigger coyote time if we just jumped
+                if (!Input.GetKey(KeyCode.Space))
+                {
+                    timeLastTouchedGround = Time.time;
+                }
+            }
+        }
+    }
+    private void PlayRandomizedJumpSound()
+    {
+        jumpAudioSource.pitch = Random.Range(0.6f, 1f);
+        jumpAudioSource.volume = Random.Range(0.6f, 1f);
+        jumpAudioSource.PlayOneShot(jumpAudioSource.clip);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,16 +9,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float moveMult = 10;
     [SerializeField]
-    private float jumpMult = 50;
-    [SerializeField]
     private MovementSpeedScaler speedScalar;
-
-    private HashSet<GameObject> groundsBeingTouched = new();
-    private bool startedAJump = false;
-    private float timeLastTouchedGround = -10f;
-    private float coyoteTimeAmount = .2f;
-
-    public AudioSource jumpAudioSource;
+    [SerializeField]
+    private GameObject feet;
 
     //[SerializeField]
     //private float gravityScale = 10;
@@ -31,15 +24,6 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space) &&
-            (groundsBeingTouched.Count > 0 || (Time.time - timeLastTouchedGround < coyoteTimeAmount)))
-        {
-            startedAJump = true;
-        }
-    }
 
     private void FixedUpdate()
     {
@@ -62,62 +46,5 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        if (startedAJump)
-        {
-			PlayRandomizedJumpSound();
-            rb.AddForce(Vector2.up * jumpMult, ForceMode2D.Impulse);
-            // Reset jump state
-            startedAJump = false;
-            timeLastTouchedGround = -10f;
-        }
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        CheckForGroundCollision(collision.gameObject);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        CheckForGroundCollision(collision.gameObject);
-    }
-
-    private void CheckForGroundCollision(GameObject gameObject)
-    {
-        if (gameObject.GetComponent<JumpableSurface>() != null)
-        {
-            groundsBeingTouched.Add(gameObject);
-        }
-    }
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        CheckForGroundExit(collision.gameObject);
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        CheckForGroundExit(collision.gameObject);
-    }
-
-    private void CheckForGroundExit(GameObject gameObject)
-    {
-        if (gameObject.GetComponent<JumpableSurface>() != null)
-        {
-            groundsBeingTouched.Remove(gameObject);
-            if (groundsBeingTouched.Count == 0)
-            {
-                //dont retrigger coyote time if we just jumped
-                if (!Input.GetKey(KeyCode.Space))
-                {
-                    timeLastTouchedGround = Time.time;
-                }
-            }
-        }
-    }
-
-    private void PlayRandomizedJumpSound()
-    {
-        jumpAudioSource.pitch = Random.Range(0.6f, 1f);
-        jumpAudioSource.volume = Random.Range(0.6f, 1f);
-        jumpAudioSource.PlayOneShot(jumpAudioSource.clip);
     }
 }
