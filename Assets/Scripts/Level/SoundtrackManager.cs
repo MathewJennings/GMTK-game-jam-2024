50f2bb66ba6558104ace09fc036fa88be0f87cce
Mathew Jennings <mathew.percussion@gmail.com>
2024-08-19 21:29:01 -0400

    Combine level 9 and 10, then insert a new Anticlimax

--- Assets/Scripts/Level/SoundtrackManager.cs
+++ Assets/Scripts/Level/SoundtrackManager.cs
@@ -7,16 +7,22 @@ public class SoundtrackManager : MonoBehaviour
 {
     [SerializeField]
     private AudioClip defaultClip;
+    [SerializeField]
+    private float defaultVolume;
 
     [SerializeField]
     private AudioClip penultimateClip;
     [SerializeField]
     private string penultimateScene;
+    [SerializeField]
+    private float penultimateVolume;
 
     [SerializeField]
     private AudioClip finalClip;
     [SerializeField]
     private string finalScene;
+    [SerializeField]
+    private float finalVolume;
 
     [SerializeField]
     private AudioSource audioSource;
@@ -27,29 +33,29 @@ public class SoundtrackManager : MonoBehaviour
         audioSource.Play();
     }
 
-    private void PlayClip(AudioClip clip)
+    private void PlayClip(AudioClip clip, float volume)
     {
+        audioSource.volume = volume;
         if (audioSource.clip != clip)
         {
             audioSource.clip = clip;
             audioSource.Play();
         }
-        
     }
 
     public void UpdateTrackForLevel(string levelName)
     {
         if (levelName == penultimateScene)
         {
-            PlayClip(penultimateClip);
+            PlayClip(penultimateClip, penultimateVolume);
         }
         else if (levelName == finalScene)
         {
-            PlayClip(finalClip);
+            PlayClip(finalClip, finalVolume);
         }
         else
         {
-            PlayClip(defaultClip);
+            PlayClip(defaultClip, defaultVolume);
         }
     }
 }
